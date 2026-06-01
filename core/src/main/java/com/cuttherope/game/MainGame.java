f5f44d6c4381bb8690d72590f99444cd2ebf042d
vale521 <valeriayserranot@gmail.com>
2026-05-31 18:55:02 -0600

    ver si si me funciona

--- core/src/main/java/com/cuttherope/game/MainGame.java
+++ core/src/main/java/com/cuttherope/game/MainGame.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Game;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MainGame extends Game {
     @Override
-    public void create()//hola camaron sin cola
+    public void create()//hola valeria
     {
         System.out.println("ENTRO A CREATE");
         setScreen(new FirstScreen(this));
