8177152a3baed3bc1ce46294bb574c4d45f8fafa
vale521 <valeriayserranot@gmail.com>
2026-06-08 18:45:20 -0600

    merge de integrarGestorNiveles

--- core/src/main/java/juego/Nivel3.java
+++ core/src/main/java/juego/Nivel3.java
@@ -9,5 +9,14 @@ public class Nivel3 extends Nivel{
     protected void configurarNivel()
     {
         System.out.println("Entro al nivel 3");
+        caramelo = new Caramelo(520,420);
+        omNom = new OmNom(700,120);
+        estrellas.add(new Estrella(220, 430));
+        estrellas.add(new Estrella(170, 250));
+        estrellas.add(new Estrella(520, 120));
+        cuerdas.add(new Cuerda(300, 620, 260, caramelo, -45));
+        cuerdas.add(new Cuerda(560, 620, 210, caramelo, 15));
+        cuerdas.add(new Cuerda(300, 300, 320, caramelo, 60));
+
     }
 }
